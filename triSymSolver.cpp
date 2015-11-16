cf9322e31db8cf5c5b6a4b0eedc13bb096aec458
asclines <injpk23@gmail.com>
2015-11-16 13:21:59 -0600

    Updated variable names cleaned up SymSolver

--- triSymSolver.cpp
+++ triSymSolver.cpp
@@ -10,31 +10,30 @@ using namespace std;
  *d - Result column
  *n - Size of matrix
  */
-void solve(double* a, double* b,double* d, int n) {
+void solve(double* b, double* a,double* d, int n) {
     n--; // since we start from x0 (not x1)
-    b[0] /= b[0];
-    d[0] /= b[0];
+    a[0] /= a[0];
+    d[0] /= a[0];
 
     for (int i = 1; i < n; i++) {
-	b[i] /= b[i] - a[i]*b[i-1];
-        d[i] = (d[i] - a[i]*d[i-1]) / (b[i] - a[i]*b[i-1]);
+	a[i] /= a[i] - b[i]*a[i-1];
+        d[i] = (d[i] - b[i]*d[i-1]) / (a[i] - b[i]*a[i-1]);
     }
 
-    d[n] = (d[n] - a[n]*d[n-1]) / (b[n] - a[n]*b[n-1]);
+    d[n] = (d[n] - b[n]*d[n-1]) / (a[n] - b[n]*a[n-1]);
 
     for (int i = n; i-- > 0;) {
-        d[i] -= b[i]*d[i+1];
+        d[i] -= a[i]*d[i+1];
     }
 }
 
 int main() {
 	int  n = 4;
-	double a[4] = { 0, -1, -1, -1 };
-	double b[4] = { 4,  4,  4,  4 };
-	double c[4] = {-1, -1, -1,  0 };
+	double b[4] = { 0, -1, -1, -1 };
+	double a[4] = { 4,  4,  4,  4 };
 	double d[4] = { 5,  5, 10, 23 };
 	// results    { 2,  3,  5, 7  }
-	solve(a,b,d,n);
+	solve(b,a,d,n);
 	for (int i = 0; i < n; i++) {
 		cout << d[i] << endl;
 	}

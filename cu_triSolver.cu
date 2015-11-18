1945399436da6d674a571c8ea72f0dd7d86780bb
asclines <injpk23@gmail.com>
2015-11-18 13:24:28 -0600

    Added cu file for solver

---
+++ cu_triSolver.cu
@@ -0,0 +1,8 @@
+#include "cu_triSolver.h"
+
+
+void triSolve(thrust::host_vector<int> sub_diag, thrust::host_vector<int> main_diag, thrust::host_vector<int> super_diag, thrust::host_vector<int> col_sol, int size){
+
+}
+
+

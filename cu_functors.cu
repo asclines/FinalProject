981a172ebbf2877a37eae42a31fd5f2d2c8bf60f
asclines <injpk23@gmail.com>
2015-11-20 11:08:05 -0600

    Added hydrogren functor

---
+++ cu_functors.cu
@@ -0,0 +1,33 @@
+/**
+* This file contains all the functors used for triSolver
+**/
+
+
+
+/**
+* returns c/(b-a*c_prime)
+**/
+struct hydrogen{
+	__host__ __device__
+	double operator()(double a, double b, double c , double c_prime){
+		
+	}
+
+};
+
+
+struct helium{
+
+
+};
+
+struct neon{
+
+
+};
+
+
+struct silicon{
+
+
+};

5b2fe2ad9e1f06dad0bba8909d6c92fa3daa6806
asclines <injpk23@gmail.com>
2015-11-30 11:22:05 -0600

    CR work

--- cyclic-reduction/cu_cr_internal.h
+++ cyclic-reduction/cu_cr_internal.h
@@ -1,8 +1,10 @@
 #ifndef CU_CR_INTERNAL_H
 #define CU_CR_INTERNAL_H
 
-#include <thrust/device_vector.h>
 #include <thrust/device_ptr.h>
+#include <thrust/device_vector.h>
+#include <thrust/iterator/zip_iterator.h>
+#include <thrust/iterator/counting_iterator.h>
 
 /*
  * 	This header file contains all foward declarations of methods used internall by
@@ -20,7 +22,23 @@ void calc_init(int n,
 	thrust::device_ptr<double> d_vect_d);
 
 
+namespace cyclic_reduction{
 
+/*
+ * 	TYPEDEFS
+ */
+	typedef thrust::device_ptr<double> DPtrD; //Device Ptr Double 
+	typedef thrust::device_vector<double> DVectorD; //Device Vector Double
+	typedef thrust::tuple<int, double> TupleID; //Tuple Integer Double
+	typedef thrust::tuple<thrust::counting_iterator<int>, DVectorD::iterator> TupleCiDvi; //Tuple Counting iterator Device vector iterator
+	typedef thrust::zip_iterator<TupleCiDvi> ZipIteratorTCD; //Zip Iterator TupleCiDvi
+
+/*
+ * 	FOWARD DELCLARATIONS
+ */
+
+
+}
 
 
 #endif
